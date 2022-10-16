ce1927b8898a3968451d2ae85cbd79415fd22cc2
msuerkan <msuerkan@partner.innova.com.tr>
2022-10-16 23:01:29 +0300

    Middle Element Of LinkedList

---
+++ src/main/java/org/example/Problem876/MiddleOfLinkedList.java
@@ -0,0 +1,51 @@
+package org.example.Problem876;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MiddleOfLinkedList {
+
+    private static ListNode middleNode(ListNode head) {
+
+       ListNode slow = head;
+         ListNode fast = head;
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode middle = middleNode(head);
+        System.out.println("middle element is " + middle.val);
+
+    }
+}
