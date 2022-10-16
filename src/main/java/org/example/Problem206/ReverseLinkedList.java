a5b8c48f580e4631391ffc958d76cd1697f503d4
msuerkan <msuerkan@partner.innova.com.tr>
2022-10-16 22:50:09 +0300

    Initial commit

---
+++ src/main/java/org/example/Problem206/ReverseLinkedList.java
@@ -0,0 +1,56 @@
+package org.example.Problem206;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+
+    private static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null,current = head,next = null;
+
+        while (current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
+
+        ListNode reversed = reverseList(head);
+        while (reversed != null) {
+            System.out.println(reversed.val);
+            reversed = reversed.next;
+        }
+    }
+
+
+}
